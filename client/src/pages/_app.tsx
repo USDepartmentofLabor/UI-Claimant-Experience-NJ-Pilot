5b7749012fe179f485515d1c88b60766b4c397b2
Brandon Lenz <brandon@truss.works>
2022-06-13 17:04:38 -0400

    Allow baseUrl imports instead of just relative paths (#135)
    Co-authored-by: Kim Allen <kim@truss.works>

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-import '../styles/styles.scss'
+import 'styles/globals.css'
+import 'styles/styles.scss'
 import type { AppProps } from 'next/app'
 import '../../i18n'
 
