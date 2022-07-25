1a2731495478aee37bb70e6dfaa2dc20d33057fa
jimmy-dehart <68395065+jimmy-dehart@users.noreply.github.com>
2022-07-25 12:15:34 -0400

    Add prefixer postcss support to storybook with postcss config file to fix storybook icon not loading bug (#254)
    * Added postcss config file to force the autoprefixer to work with storybook.
    
    * Updated configuration to directly intercept the webpack loader rules and modify the postcss loader to also have some actual configuration!
    
    * Removed unnecessary modules and lint rules
    
    * Made postcss-preset-env module a dev dependency

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -21,4 +21,36 @@ module.exports = {
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
+  webpackFinal: async (config) => {
+    config.module.rules.forEach((rule) => {
+      rule.use?.forEach((loader, i) => {
+        loader === 'postcss-loader' &&
+          // eslint-disable-next-line security/detect-object-injection
+          (rule.use[i] = {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                        grid: 'autoplace',
+                      },
+                      stage: 3,
+                      features: {
+                        'custom-properties': false,
+                      },
+                    },
+                  ],
+                ],
+              },
+            },
+          })
+      })
+    })
+
+    return config
+  },
 }
