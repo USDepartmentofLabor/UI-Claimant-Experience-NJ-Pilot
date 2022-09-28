3c72d22de7baa4f45facb1c16cc544724aafb968
Kim Allen <kim@truss.works>
2022-09-28 09:25:55 -0700

    Create 'save completed claim' endpoint (#539)
    * Add completed claim controller and test replicating partial claim
    
    * Fix controller class for test
    
    * Add TODO for future story adding validation
    
    Co-authored-by: Brandon Lenz <brandon@truss.works>

---
+++ server/src/main/java/nj/lwd/ui/claimantintake/controller/CompletedClaimController.java
@@ -0,0 +1,40 @@
+package nj.lwd.ui.claimantintake.controller;
+
+import java.util.Map;
+import nj.lwd.ui.claimantintake.service.ClaimStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/completed-claim")
+public class CompletedClaimController {
+
+    private final ClaimStorageService claimStorageService;
+
+    @Autowired
+    public CompletedClaimController(ClaimStorageService claimStorageService) {
+        this.claimStorageService = claimStorageService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> saveCompletedClaim(
+            @RequestBody Map<String, Object> completedClaimPayload) {
+        // TODO: Get claimant id from session and use that instead of the idp_id when auth is in
+        //       place. For now, just make up a static IdpId and use that for all claims
+        String claimantIdpId = "test_id";
+
+        // TODO: validate claim against the schema
+        var saveStatus = claimStorageService.saveClaim(claimantIdpId, completedClaimPayload);
+
+        if (saveStatus) {
+            return new ResponseEntity<>("Save successful", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Save failed", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
