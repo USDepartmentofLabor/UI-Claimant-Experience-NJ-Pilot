ed8f4e926328c75ecc8997667c7e08ef89f502fb
Josh Clarridge <2491291+jsclarridge@users.noreply.github.com>
2022-09-29 08:12:09 -0700

    Add lighthouse (#502)
    * Add lighthouse
    
    * Add lighthouse audit
    
    * Ignore cypress videos and screenshots
    
    * Modify ci-up to rebuild images
    
    * Lighthouse test with default thresholds
    
    * Revert 'Lighthouse test with default thresholds'
    
    Co-authored-by: jimmy-dehart <68395065+jimmy-dehart@users.noreply.github.com>
    Co-authored-by: Brandon Lenz <brandon@truss.works>

--- e2e/cypress/e2e/claim-form-complete.cy.ts
+++ e2e/cypress/e2e/claim-form-complete.cy.ts
@@ -35,6 +35,13 @@ context('Initial Claim form', { scrollBehavior: 'center' }, () => {
       }
     )
     cy.checkA11y()
+    cy.lighthouse({
+      accessibility: 100,
+      'best-practices': 90,
+      seo: 90,
+      pwa: 20,
+      performance: 60,
+    })
     cy.clickNext()
 
     // Contact page
