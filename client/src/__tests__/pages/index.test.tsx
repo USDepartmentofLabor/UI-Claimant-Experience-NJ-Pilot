3793b5970ea1e604872235d0390db57916d4058c
Alysia Broddrick <abroddrick@truss.works>
2022-10-06 10:37:48 -0700

    changed home page to be the index and removed the react backsplash

--- client/src/__tests__/pages/index.test.tsx
+++ client/src/__tests__/pages/index.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import Home from 'pages/home'
+import Home from 'pages'
 
 describe('home page', () => {
   it('renders without error', () => {
