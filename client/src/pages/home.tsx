7c7d9873f521c5744ecbe7bebe50de8fa7b94ff1
laadeha <james.dehart@dol.nj.gov>
2022-10-11 11:30:43 -0400

    Fixed controller routing name

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { Button } from '@trussworks/react-uswds'
 import { useRouter } from 'next/router'
 import { pageDefinitions } from 'constants/pages/pageDefinitions'
+import { Alert } from '@trussworks/react-uswds'
 
 const Home: NextPage = () => {
   const router = useRouter()
@@ -16,6 +17,10 @@ const Home: NextPage = () => {
         <title>{t('page_title')}</title>
       </Head>
       <h1>{t('heading')}</h1>
+      {router.query?.completed && (
+        <Alert type="success" heading="Claim Completed" headingLevel="h4" />
+      )}
+
       <Button type="button" onClick={goToFirstPageOfClaimForm}>
         Press Me
       </Button>
