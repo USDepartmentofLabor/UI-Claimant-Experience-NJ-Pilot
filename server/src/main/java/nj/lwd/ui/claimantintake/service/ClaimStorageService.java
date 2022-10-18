a7478f975665587bcd46488035259fa639440cb6
Kim Allen <kim@truss.works>
2022-10-18 14:11:42 -0400

    Add COMPLETE_FAILED event to error handling

--- server/src/main/java/nj/lwd/ui/claimantintake/service/ClaimStorageService.java
+++ server/src/main/java/nj/lwd/ui/claimantintake/service/ClaimStorageService.java
@@ -96,6 +96,7 @@ public class ClaimStorageService {
                     claim.getId(),
                     e.getMessage());
             claim.addEvent(new ClaimEvent(ClaimEventCategory.SAVE_FAILED));
+            claim.addEvent(new ClaimEvent(ClaimEventCategory.COMPLETE_FAILED));
             claimantRepository.save(claimant);
             return false;
         } catch (AwsServiceException e) {
@@ -104,6 +105,7 @@ public class ClaimStorageService {
                     claim.getId(),
                     e.getMessage());
             claim.addEvent(new ClaimEvent(ClaimEventCategory.SAVE_FAILED));
+            claim.addEvent(new ClaimEvent(ClaimEventCategory.COMPLETE_FAILED));
             claimantRepository.save(claimant);
             return false;
         } catch (SdkClientException e) {
@@ -113,6 +115,7 @@ public class ClaimStorageService {
                     claim.getId(),
                     e.getMessage());
             claim.addEvent(new ClaimEvent(ClaimEventCategory.SAVE_FAILED));
+            claim.addEvent(new ClaimEvent(ClaimEventCategory.COMPLETE_FAILED));
             claimantRepository.save(claimant);
             return false;
         }
