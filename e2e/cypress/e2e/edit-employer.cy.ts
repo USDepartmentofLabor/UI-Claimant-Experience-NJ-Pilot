42928ea779939c271a0f8be34afd5d89780fc416
Alysia Broddrick <abroddrick@truss.works>
2022-11-10 10:39:48 -0800

    added cypress testing for edit employer component

---
+++ e2e/cypress/e2e/edit-employer.cy.ts
@@ -0,0 +1,7 @@
+describe('Edit employer checks', () => {
+  it('edit employer loads', () => {
+    cy.visit('/dev/edit-employer')
+    cy.get('h1').contains('Test Edit Employer Component')
+    cy.get('[data-testid=edit-employer-test-subheader]').should('be.visible')
+  })
+})
