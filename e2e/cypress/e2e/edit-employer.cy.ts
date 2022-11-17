5a05e0c5384836254735fdc3688174cae8e167da
Alysia Broddrick <abroddrick@truss.works>
2022-11-17 13:59:09 -0800

    added cypress test

--- e2e/cypress/e2e/edit-employer.cy.ts
+++ e2e/cypress/e2e/edit-employer.cy.ts
@@ -1,3 +1,4 @@
+import fillChangeInEmployment from './formPageFilling/changeInEmployment'
 import fillYourEmployerFields from './formPageFilling/yourEmployer'
 
 describe('Edit employer checks', () => {
@@ -9,6 +10,14 @@ describe('Edit employer checks', () => {
   it('fills out edit employer completely', () => {
     const employerIndexString = '0'
     fillYourEmployerFields(employerIndexString, { is_full_time: true })
+
+    fillChangeInEmployment(employerIndexString, {
+      separation_circumstance: 'laid_off',
+
+      employment_start_date: { mo: '02', day: '20', yr: '2014' },
+      employment_last_date: { mo: '02', day: '24', yr: '2022' },
+      expect_to_be_recalled: false,
+    })
     /* add other edit employer cypress tests here */
   })
 })
