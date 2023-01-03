ee16bbe2e7b5a4aa1350189f7974c4a8e1e8cc9e
Josh Clarridge <2491291+jsclarridge@users.noreply.github.com>
2023-01-03 15:02:39 -0800

    Revert 'Update default privileges for db_migration_user' (#1182)

--- server/src/main/resources/db/changelog/changes/20221228162934_update_db_migration_default_privileges.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset jsclarridge:1672273774400
-
-ALTER DEFAULT PRIVILEGES FOR ROLE db_migration_user IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE db_migration_user IN SCHEMA public GRANT USAGE ON SEQUENCES TO app_role;
