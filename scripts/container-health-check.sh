ba00aba46f92c8e28f15253de35326a48b9541c8
ralren <ren@truss.works>
2023-01-20 14:07:01 -0800

    try to wait for containers to be healthy after spinning up

---
+++ scripts/container-health-check.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Checks and waits for the docker containers to be healthy
+# Adapted from https://github.com/raschmitt/wait-for-healthy-container/blob/master/wait-for-healthy-container.sh
+
+set -eu
+
+container_name=$1
+shift
+timeout=$1
+
+default_timeout=120
+if [ -z "${timeout}" ]; then
+    timeout=${default_timeout}
+fi
+
+echo "Waiting for ${container_name} to be healthy..."
+for i in $(seq "${timeout} "); do
+    state=$(docker inspect -f '{{ .State.Health.Status }}' "${container_name}")
+    if [ "${state}" -eq 0 ]; then
+        echo "${container_name} is healthy after ${i} seconds."
+        exit 0
+    fi
+    sleep 1
+done
+
+echo "Timeout exceeded. Health status returned: $(docker inspect -f '{{ .State.Health.Status }}' "${container_name} ")"
+exit 1
