3e43fad19dfc007ec1832019cc0eb35999640e9c
barkleyx <barkleyx2@gmail.com>
2023-02-01 09:53:35 -0500

    Employer phone number verified field (#1390)
    * Update format display phone number
    
    * Update Jest test
    
    ---------
    
    Co-authored-by: Barkley <bxu@njit.edu>

--- client/src/utils/phone/format.test.ts
+++ client/src/utils/phone/format.test.ts
@@ -1,9 +1,13 @@
 import { formatStoredToDisplayPhone } from './format'
 
 describe('formatStoredToDisplayPhone', () => {
-  it('returns the expected format', () => {
+  it('returns the expected format when at length 10', () => {
     const formattedPhone = formatStoredToDisplayPhone('1234567891')
     expect(formattedPhone).toEqual('123-456-7891')
+  })
+  it('returns the unformated phone number when not at length 10', () => {
+    const formattedPhone = formatStoredToDisplayPhone('12345678910')
+    expect(formattedPhone).toEqual('12345678910')
   })
   it('returns undefined if argument is undefined', () => {
     const formattedPhone = formatStoredToDisplayPhone(undefined)
