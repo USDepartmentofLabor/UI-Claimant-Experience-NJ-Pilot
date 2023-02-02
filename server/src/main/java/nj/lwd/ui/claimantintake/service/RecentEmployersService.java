39d32ff6d5e0159484c657d44b02e01f0b70c78b
Alysia Broddrick <abroddrick@truss.works>
2023-02-02 11:08:20 -0800

    fixed spotsbug issue

--- server/src/main/java/nj/lwd/ui/claimantintake/service/RecentEmployersService.java
+++ server/src/main/java/nj/lwd/ui/claimantintake/service/RecentEmployersService.java
@@ -23,19 +23,28 @@ public class RecentEmployersService {
     @Autowired
     public RecentEmployersService(Environment environment) {
 
-        String endpointURL = environment.getProperty("loops.url");
-        logger.debug("recent employer service endpoint is ", endpointURL);
-        this.webClient =
-                WebClient.builder()
-                        .baseUrl(endpointURL)
-                        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .build();
+        if (environment == null) {
+            this.webClient = null;
+        } else {
+            String endpointURL = environment.getProperty("loops.url") + "";
+            logger.debug("recent employer service endpoint is ", endpointURL);
+            this.webClient =
+                    WebClient.builder()
+                            .baseUrl(endpointURL)
+                            .defaultHeader(
+                                    HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                            .build();
+        }
     }
 
     public RecentEmployersResponse getRecentEmployerValues(String ssn, String claimDate)
             throws WGPMClientException, WGPMServerException {
         logger.debug("requesting recent employers");
         WagePotentialMonLookupRequest request = new WagePotentialMonLookupRequest(ssn, claimDate);
+        if (webClient == null) {
+            logger.error("Webclient is null, endpoint may invalid");
+            throw new WGPMServerException("Unable to create connection to the server");
+        }
         return webClient
                 .post()
                 .uri("")
