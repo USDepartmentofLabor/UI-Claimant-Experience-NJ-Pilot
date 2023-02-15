64feaaf421fd2ed127daf5bad1de6dc80213dcb6
andy <andy@andyhub.com>
2023-02-15 08:25:51 -0500

    definitely fixed cypress this time

--- e2e/cypress/e2e/formPageFilling/screener.ts
+++ e2e/cypress/e2e/formPageFilling/screener.ts
@@ -4,7 +4,7 @@ const fillScreenerFields = () => {
     .parent()
     .click()
   cy.get('input[id=screener_job_last_eighteen_months\\.yes]').parent().click()
-  cy.get('input[id=screener_work_nj\\.options\\.both]').parent().click()
+  cy.get('input[id=screener_work_nj\\.both]').parent().click()
   cy.get('input[id=screener_currently_disabled\\.no]').parent().click()
   cy.get('input[id=screener_federal_work_in_last_eighteen_months\\.no')
     .parent()
