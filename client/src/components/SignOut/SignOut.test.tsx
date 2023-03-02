babd7b5bfd77dc67864ad0b721caee05f7d198b0
Alysia Broddrick <abroddrick@truss.works>
2023-03-02 08:13:02 -0800

    client/src/components/SignOut/SignOut.test.tsx

--- client/src/components/SignOut/SignOut.test.tsx
+++ client/src/components/SignOut/SignOut.test.tsx
@@ -12,20 +12,19 @@ describe('SignOut', () => {
     mockCognitoSignOut.mockClear()
   })
 
-  //fix here
-  // it('Renders sign out button and calls mockCognitoSignOut', async () => {
-  //   const user = userEvent.setup()
-  //   render(<SignOut isNavLink={false} />)
+  it('Renders sign out button and calls mockCognitoSignOut', async () => {
+    const user = userEvent.setup()
+    render(<SignOut isNavLink={false} />)
 
-  //   const signOutButton = screen.queryByText('Sign out')
+    const signOutButton = screen.queryByText('signout')
 
-  //   expect(signOutButton).toBeInTheDocument()
-  //   expect(signOutButton).toHaveAttribute('type', 'button')
+    expect(signOutButton).toBeInTheDocument()
+    expect(signOutButton).toHaveAttribute('type', 'button')
 
-  //   await user.click(signOutButton as HTMLElement)
+    await user.click(signOutButton as HTMLElement)
 
-  //   expect(mockCognitoSignOut).toHaveBeenCalledTimes(1)
-  // })
+    expect(mockCognitoSignOut).toHaveBeenCalledTimes(1)
+  })
 
   it('Renders log out nav item  and calls mockCognitoSignOut', async () => {
     const user = userEvent.setup()
