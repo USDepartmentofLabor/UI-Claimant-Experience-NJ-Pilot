489edf80f3b060531ec6701a46ba261e5a3f004c
Alysia Broddrick <abroddrick@truss.works>
2023-03-02 08:25:24 -0800

    removed extra line

--- client/src/i18n/en/home.ts
+++ client/src/i18n/en/home.ts
@@ -1,7 +1,6 @@
 const home = {
   page_title: 'Apply for Unemployment Insurance',
   heading: 'Apply for Unemployment Insurance',
-
   file_a_claim_button: 'File a claim',
   continue_claim_button: 'Continue claim',
 }
