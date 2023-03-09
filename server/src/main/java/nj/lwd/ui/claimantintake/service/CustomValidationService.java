72cc97e53d44012a975e5900e80732183d3d1d87
Alysia Broddrick <abroddrick@truss.works>
2023-03-08 16:28:02 -0800

    added service tests

--- server/src/main/java/nj/lwd/ui/claimantintake/service/CustomValidationService.java
+++ server/src/main/java/nj/lwd/ui/claimantintake/service/CustomValidationService.java
@@ -1,40 +1,132 @@
 package nj.lwd.ui.claimantintake.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CustomValidationService {
-    private ArrayList<String> validationErrors;
 
-    CustomValidationService() {
-        validationErrors = new ArrayList<>();
-    }
+    private final int MAX_MAILING_ADDRESS_LEN = 44;
+    private final String SSN_FOURTH_FIFTH_CHARS = "00";
+    private final String SSN_SIXTH_NINETH_CHARS = "0000";
+    private final String MAILING_ADDRESS_ERROR =
+            String.format(
+                    "Mailing address error: street and city fields exceed the %d character maximum",
+                    MAX_MAILING_ADDRESS_LEN);
+    private final String SSN_FOURTH_FIFTH__ERROR =
+            String.format(
+                    "SSN error: the 4th and 5th characters cannot equal %s",
+                    SSN_FOURTH_FIFTH_CHARS);
+    private final String SSN_SIXTH_SEVENTH_ERROR =
+            String.format(
+                    "SSN error: the 6th and 9th characters cannot equal %s",
+                    SSN_SIXTH_NINETH_CHARS);
+    private final String LAST_DATE_ERROR =
+            "Employment last date error: last date cannot be before employment start date";
+    private final String RECALL_DATE_ERROR =
+            "Definite date of recall error: date of recall cannot be before employment last date";
+    private final Logger logger = LoggerFactory.getLogger(CustomValidationService.class);
+
+    CustomValidationService() {}
 
     public ArrayList<String> performCustomValidations(Map<String, Object> claimData) {
-        // make running arraylist
 
-        // check residence address peices-add to list using passing by reference if error
+        ArrayList<String> validationErrors = new ArrayList<>();
 
-        // check mailing address
-        // check ssn
-        // check defindate date of recall is after last date
-        // check last date is different from start date
+        validateMailingAddress(validationErrors, claimData.get("mailing_address"));
+        validateSSN(validationErrors, (String) claimData.get("ssn"));
+        validateLastDateAfterStartDate(
+                validationErrors,
+                (String) claimData.get("employment_start_date"),
+                (String) claimData.get("employment_last_date"));
+        validateRecallDateAfterLastDate(
+                validationErrors,
+                (String) claimData.get("definite_recall_date"),
+                (String) claimData.get("employment_last_date"));
         return validationErrors;
     }
 
-    private void validateMailingAddress() {}
+    private boolean dateIsAfter(String earlierDateString, String laterDateString) {
+        if (earlierDateString == null || laterDateString == null) {
+            // do not throw an error for missing field
+            return true;
+        }
 
-    private void validateSSN() {}
+        LocalDate earlierDate = LocalDate.parse(earlierDateString);
+        LocalDate laterDate = LocalDate.parse(laterDateString);
 
-    private void validateSSN() {}
+        return laterDate.isAfter(earlierDate);
+    }
 
-    private void validateSSN() {}
+    private void validateMailingAddress(
+            ArrayList<String> validationErrors, Object mailingAddressObj) {
+        int addressLen = 0;
+        int cityLen = 0;
+        if (mailingAddressObj != null) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, Object> addressfields = new HashMap<String, Object>();
+                addressfields =
+                        (Map<String, Object>)
+                                objectMapper.convertValue(
+                                        mailingAddressObj, addressfields.getClass());
+                System.out.println(addressfields.toString());
+                String address = (String) addressfields.get("address");
+                String city = (String) addressfields.get("city");
+                if (address != null) {
+                    addressLen = address.length();
+                }
 
-    private void validateSSN() {}
+                if (city != null) {
+                    cityLen = city.length();
+                }
 
-    private void addError(String error) {
-        if (error != null) {
-            validationErrors.add(error);
+                if (addressLen + cityLen > MAX_MAILING_ADDRESS_LEN) {
+                    validationErrors.add(MAILING_ADDRESS_ERROR);
+                }
+            } catch (IllegalArgumentException e) {
+                logger.error(
+                        "Mailing address was unable to be converted to a map but was not null.");
+            }
+        }
+    }
+
+    private void validateSSN(ArrayList<String> validationErrors, String ssn) {
+        if (!(ssn == null || ssn.length() != 9)) {
+            String fourthFifthChars = ssn.substring(3, 5);
+            String sixthSeventhChars = ssn.substring(5, 9);
+
+            if (fourthFifthChars.equals(SSN_FOURTH_FIFTH_CHARS)) {
+
+                validationErrors.add(SSN_FOURTH_FIFTH__ERROR);
+            }
+
+            if (sixthSeventhChars.equals(SSN_SIXTH_NINETH_CHARS)) {
+                validationErrors.add(SSN_SIXTH_SEVENTH_ERROR);
+            }
+        }
+    }
+
+    // check defindate date of recall is after last date
+    private void validateRecallDateAfterLastDate(
+            ArrayList<String> validationErrors,
+            String definiteRecallDateString,
+            String employmentLastDateString) {
+        if (!dateIsAfter(employmentLastDateString, definiteRecallDateString)) {
+            validationErrors.add(RECALL_DATE_ERROR);
+        }
+    }
+
+    private void validateLastDateAfterStartDate(
+            ArrayList<String> validationErrors,
+            String employmentStartDateString,
+            String employmentLastDateString) {
+        if (!dateIsAfter(employmentStartDateString, employmentLastDateString)) {
+            validationErrors.add(LAST_DATE_ERROR);
         }
     }
 }
---
+++ server/src/test/java/nj/lwd/ui/claimantintake/service/CustomValidationServiceTest.java
@@ -0,0 +1,151 @@
+package nj.lwd.ui.claimantintake.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomValidationServiceTest {
+    private CustomValidationService customValidationService;
+
+    @BeforeEach
+    void beforeEach() {
+        customValidationService = new CustomValidationService();
+    }
+
+    @Test
+    void noErrorsResultWithValidClaim() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> validClaim =
+                objectMapper.readValue(
+                        """
+                                {
+                                    "employment_last_date":"2023-03-01",
+                                    "employment_start_date":"2003-12-02",
+                                    "definite_recall_date":"2023-12-03",
+                                    "ssn":"987654321",
+                                    "Misc_additionalField":"some other data",
+                                  "mailing_address": {
+                                    "address": "123 main street",
+                                    "city": "San Francisco",
+                                    "state": "CA",
+                                    "zipcode": "12345"
+                                  }
+                                }
+                                """,
+                        new TypeReference<>() {});
+        ArrayList<String> errors = customValidationService.performCustomValidations(validClaim);
+        System.out.println(errors);
+        assertEquals(0, errors.size());
+    }
+
+    @Test
+    void doesntReturnErrorsForMissingFields() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> claimMissingFields =
+                objectMapper.readValue(
+                        """
+                  {
+                    "Misc_additionalField":"some other data"
+
+                }
+                                    """,
+                        new TypeReference<>() {});
+        ArrayList<String> errors =
+                customValidationService.performCustomValidations(claimMissingFields);
+        System.out.println(errors);
+        assertEquals(0, errors.size());
+    }
+
+    @Test
+    void returnsDateErrors() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> claimWithBadDates =
+                objectMapper.readValue(
+                        """
+                                {
+                                    "employment_last_date":"2023-12-01",
+                                    "employment_start_date":"2023-12-01",
+                                    "definite_recall_date":"2023-03-03"
+
+                                }
+                                """,
+                        new TypeReference<>() {});
+
+        ArrayList<String> errors =
+                customValidationService.performCustomValidations(claimWithBadDates);
+        System.out.println(errors);
+
+        assertEquals(2, errors.size());
+        assertTrue(
+                errors.indexOf(
+                                "Employment last date error: last date cannot be before employment"
+                                        + " start date")
+                        > -1);
+        assertTrue(
+                errors.indexOf(
+                                "Definite date of recall error: date of recall cannot be before"
+                                        + " employment last date")
+                        > -1);
+    }
+
+    @Test
+    void returnsAddressTooLongError() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> claimWithMailingAddress =
+                objectMapper.readValue(
+                        """
+                                {
+                                  "mailing_address": {
+                                    "address": "123 main street",
+                                    "city": "MY CAT WAS ON MY KEYBOARD AND NOW THIS TEXT IS WAYYYY TOO LONG",
+                                    "state": "CA",
+                                    "zipcode": "12345"
+                                  }
+
+                                }
+                                """,
+                        new TypeReference<>() {});
+
+        ArrayList<String> errors =
+                customValidationService.performCustomValidations(claimWithMailingAddress);
+        System.out.println(errors);
+
+        assertEquals(1, errors.size());
+        assertEquals(
+                "Mailing address error: street and city fields exceed the 44 character maximum",
+                errors.get(0));
+    }
+
+    @Test
+    void returnsSSNErrors() throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> validClaim =
+                objectMapper.readValue(
+                        """
+                              {
+                                  "ssn":"987000000"
+                              }
+                              """,
+                        new TypeReference<>() {});
+        ArrayList<String> errors = customValidationService.performCustomValidations(validClaim);
+        System.out.println(errors);
+        assertEquals(2, errors.size());
+
+        assertTrue(errors.indexOf("SSN error: the 4th and 5th characters cannot equal 00") > -1);
+        assertTrue(errors.indexOf("SSN error: the 6th and 9th characters cannot equal 0000") > -1);
+    }
+}
