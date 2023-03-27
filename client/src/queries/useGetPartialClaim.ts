7be0edba43e97582cde365a209f060b89d7f588b
Alysia Broddrick <abroddrick@truss.works>
2023-03-27 14:06:25 -0700

    removed console log

--- client/src/queries/useGetPartialClaim.ts
+++ client/src/queries/useGetPartialClaim.ts
@@ -7,7 +7,6 @@ import { PartialClaimResponseType } from 'types/ResponseTypes'
 import { useSession } from 'next-auth/react'
 
 const getPartialClaim = async () => {
-  // console.log("**performing get partial claim")
   try {
     const { data } = await serverHttpClient.get<PartialClaimResponseType>(
       '/partial-claim',
