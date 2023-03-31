4ac761bba71316b0bddc589de01cabbe386fb7e1
mhadfieldNavaPBC <michellehadfield@navapbc.com>
2023-03-31 13:56:20 -0600

    updating useGetVerifiedAddress.test.tsx

--- client/src/queries/useGetVerifiedAddress.test.tsx
+++ client/src/queries/useGetVerifiedAddress.test.tsx
@@ -1,14 +1,15 @@
 import { renderHook, waitFor } from '@testing-library/react'
 import { useGetVerifiedAddress } from './useGetVerifiedAddress'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { AddressInput } from '../types/claimantInput'
+import {
+  CORRECTED_ADDRESS,
+  NO_PARAMS_ERROR,
+} from '../constants/api/services/verifyAddress'
+import axios from 'axios'
 
-const mockAxiosGet = jest
-  .fn()
-  .mockImplementation(() => ({ data: { status: 200 } }))
-jest.mock('axios', () => ({
-  get: mockAxiosGet,
-  isAxiosError: () => true,
-}))
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
 
 const wrapper = ({ children }: any) => {
   return (
@@ -17,36 +18,49 @@ const wrapper = ({ children }: any) => {
     </QueryClientProvider>
   )
 }
-const requestParams = {
+const requestParams: AddressInput = {
   address: '1234 Michael Scott St',
   address2: 'Dunder Mifflin suite',
   city: 'Scranton',
   state: 'PA',
   zipcode: '18510',
 }
-const getData = {
-  address: {
-    address: '1234 E Michael Scott St',
-    address2: 'Dunder Mifflin Suite',
-    city: 'Scranton',
-    state: 'PA',
-    zipcode: '18510-1234',
+const successResponse = {
+  response: {
+    address: {
+      address: '1234 E MICHAEL SCOTT ST',
+      address2: 'DUNDER MIFFLIN STE',
+      city: 'SCRANTON',
+      state: 'PA',
+      zipcode: '18510-1234',
+    },
+    errorSummary: CORRECTED_ADDRESS,
   },
-  errorSummary: '',
+  status: 200,
 }
-describe('use get verified address', () => {
+const emptyParamsResponse = {
+  response: {
+    NO_PARAMS_ERROR,
+  },
+  status: 200,
+}
+describe('use get verified address hook works as expected', () => {
   it('calls the query and gets back data', async () => {
-    mockAxiosGet.mockResolvedValueOnce({ data: getData })
+    mockedAxios.get.mockResolvedValueOnce(successResponse)
 
     const { result } = renderHook(() => useGetVerifiedAddress(requestParams), {
       wrapper,
     })
     await waitFor(() => expect(result.current.isSuccess).toEqual(true))
-    expect(result.current.isSuccess).toBeTruthy()
-    expect(mockAxiosGet).toHaveBeenCalledTimes(0)
-    expect(result.current.data).not.toEqual(getData)
-    const refetchResult = await result.current.refetch()
+    expect(result.current.data).toEqual(successResponse)
+  })
+  it('calls the query without params and gets back the expected error', async () => {
+    mockedAxios.get.mockResolvedValueOnce(emptyParamsResponse)
 
-    expect(refetchResult.isSuccess).toBeTruthy()
+    const { result } = renderHook(() => useGetVerifiedAddress(undefined), {
+      wrapper,
+    })
+    await waitFor(() => expect(result.current.isSuccess).toEqual(true))
+    expect(result.current.data).toEqual(emptyParamsResponse)
   })
 })
