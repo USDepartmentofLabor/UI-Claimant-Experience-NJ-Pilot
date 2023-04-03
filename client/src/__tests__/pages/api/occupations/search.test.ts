78f930986cd5d5438f923e03669e243a9f09f6b8
Sawyer Hollenshead <git@sawyerh.com>
2023-04-03 16:01:33 -0700

    Add Occucoder service and endpoint: POST /api/occupations/search (#1991)
    Resolves #1859
    
    ## Changes proposed in this pull request
    
    - Adds new `Occucoder` service class
    - Adds new `POST /api/occupations/search` endpoint for finding Occupation codes for a user-entered job title/description

---
+++ client/src/__tests__/pages/api/occupations/search.test.ts
@@ -0,0 +1,116 @@
+/**
+ * @jest-environment node
+ */
+import { Occucoder } from 'services/Occucoder'
+import { getServerSession } from 'next-auth/next'
+import handler from 'pages/api/occupations/search'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { AxiosError, AxiosResponse } from 'axios'
+
+jest.mock('next-auth/next')
+jest.mock('services/Occucoder')
+
+const mockGetServerSession = getServerSession as jest.MockedFunction<
+  typeof getServerSession
+>
+
+function getMockRequest(body = { job_title: 'Software Engineer' }) {
+  return {
+    body,
+  } as NextApiRequest
+}
+
+function getMockResponse() {
+  return {
+    json: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown as NextApiResponse
+}
+
+function mockLoggedInSession() {
+  mockGetServerSession.mockResolvedValueOnce({ accessToken: 'mock' })
+}
+
+describe('POST /api/occupations/search', () => {
+  it('responds with occupation results', async () => {
+    const req = getMockRequest()
+    const res = getMockResponse()
+
+    const mockOccupations = [
+      {
+        job_title: 'Software Engineer',
+        job_description: 'Software Engineer',
+        job_code: '15-1132',
+        score: 100,
+      },
+    ]
+
+    mockLoggedInSession()
+    jest
+      .spyOn(Occucoder.prototype, 'getOccupationCodes')
+      .mockResolvedValue(mockOccupations)
+
+    await handler(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith(mockOccupations)
+  })
+
+  it('responds with 401 when not authenticated', async () => {
+    const req = getMockRequest()
+    const res = getMockResponse()
+
+    mockGetServerSession.mockResolvedValueOnce(null)
+
+    await handler(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(401)
+  })
+
+  it('responds with 401 when job title is blank space', async () => {
+    const req = getMockRequest({ job_title: ' ' })
+    const res = getMockResponse()
+
+    mockLoggedInSession()
+
+    await handler(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(401)
+  })
+
+  it('responds with 500 when Occucoder service throw a generic error', async () => {
+    const req = getMockRequest()
+    const res = getMockResponse()
+
+    mockLoggedInSession()
+    jest
+      .spyOn(Occucoder.prototype, 'getOccupationCodes')
+      .mockRejectedValue(new Error('mock'))
+    // Error is intentionally logged for this scenario
+    jest.spyOn(console, 'error').mockImplementation()
+
+    await handler(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+  })
+
+  it('responds with the Occucoder error response status and message', async () => {
+    const req = getMockRequest()
+    const res = getMockResponse()
+
+    mockLoggedInSession()
+
+    jest.spyOn(Occucoder.prototype, 'getOccupationCodes').mockRejectedValue(
+      new AxiosError('Mocked error', '400', undefined, undefined, {
+        status: 400,
+        data: 'Mocked error response body',
+      } as AxiosResponse)
+    )
+
+    await handler(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.send).toHaveBeenCalledWith('Mocked error response body')
+  })
+})
