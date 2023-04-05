2e5cc0b438582e9bf69e84b0eb8c0dfc5722a9ff
mhadfieldNavaPBC <michellehadfield@navapbc.com>
2023-04-05 11:39:12 -0600

    changed request parameter handling

--- client/src/queries/useGetVerifiedAddress.tsx
+++ client/src/queries/useGetVerifiedAddress.tsx
@@ -1,29 +1,14 @@
 import { useQuery } from 'react-query'
-import axios, { AxiosError, AxiosResponse } from 'axios'
-import { AddressInput } from '../types/claimantInput'
+import axios from 'axios'
+import { AddressInput } from 'types/claimantInput'
 
 const getVerifiedAddress = async (address: AddressInput | undefined) => {
-  const addressParams = convertJSONAddressToURLParams(address)
-  const response = await axios.get(
-    '/api/services/verify-address?' + addressParams
-  )
-  return response
+  //TODO MRH discuss if better ot use POST and send object to service or do the conversion of params here or sooner
+  return await axios.post('/api/services/verify-address', address)
 }
 
 export const useGetVerifiedAddress = (address: AddressInput | undefined) => {
-  //using the first line of the address as a key to differentiate requests and responses
-  return useQuery<AxiosResponse<any, any> | undefined, AxiosError<any>>(
-    address?.address || 'getVerifiedAddress',
-    () => getVerifiedAddress(address)
+  return useQuery(['getVerifiedAddress', address], () =>
+    getVerifiedAddress(address)
   )
-}
-
-const convertJSONAddressToURLParams = (
-  params: AddressInput | undefined
-): string => {
-  let urlParams = new URLSearchParams(params).toString()
-  urlParams = urlParams.replace('address', 'street')
-  urlParams = urlParams.replace('address2', 'street2')
-  urlParams = urlParams.replace('zipcode', 'zip')
-  return urlParams
 }
