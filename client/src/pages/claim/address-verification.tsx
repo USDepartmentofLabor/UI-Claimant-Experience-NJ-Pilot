12b17c43e7f3a5f8361b1952a9a802c03b9ee389
mhadfieldNavaPBC <michellehadfield@navapbc.com>
2023-04-17 11:08:15 -0600

    restored error states after merge issues

--- client/src/pages/claim/address-verification.tsx
+++ client/src/pages/claim/address-verification.tsx
@@ -13,11 +13,13 @@ import {
   pageDefinitions,
 } from 'constants/pages/pageDefinitions'
 import { ADDRESS_SKELETON } from 'constants/initialValues'
-import { useTranslation } from 'next-i18next'
+import { Trans, useTranslation } from 'next-i18next'
 import { RadioField } from '../../components/form/fields/RadioField/RadioField'
 import { useFormikContext } from 'formik'
 import { useVerifiedAddress } from '../../queries/useVerifiedAddress'
 import Spinner from 'components/Spinner/Spinner'
+import { CORRECTED_ADDRESS } from '../../constants/api/services/verifyAddress'
+import { Alert, Card, CardGroup } from '@trussworks/react-uswds'
 
 const pageDefinition = AddressVerificationPageDefinition
 const nextPage = getNextPage(pageDefinition)
@@ -81,6 +83,7 @@ function AddressVerificationFeedback() {
     } // otherwise make no changes to preserve input from previous screen despite error here
   }
 
+  //not checking for error on either response as the default case is to use the entered address
   const {
     isLoading: isVerifiedResidenceAddressLoading,
     data: verifiedResidenceAddressData,
@@ -126,26 +129,76 @@ function AddressVerificationFeedback() {
   if (isVerifiedResidenceAddressLoading || isVerifiedMailingAddressLoading) {
     return <Spinner data-testid={'address-verification-spinner'} />
   }
+  //only allow selecting an address if the residence and mailing are the same or both addresses have proposed corrections
+  // this directs the claimant to the default case below where one, the other, or both addresses need revision
+  if (
+    (verifiedResidenceAddressData?.validationSummary === CORRECTED_ADDRESS &&
+      values.LOCAL_mailing_address_same) ||
+    (verifiedResidenceAddressData?.validationSummary === CORRECTED_ADDRESS &&
+      verifiedMailingAddressData?.validationSummary === CORRECTED_ADDRESS)
+  ) {
+    return (
+      <>
+        <AddressSelector
+          handleChangeAddress={handleResidenceAddressChange}
+          name={'LOCAL_residence_address_verification_selection'}
+          legend={t('address_verification.legend.residence')}
+          options={residenceAddressOptions}
+        />
+        {!values.LOCAL_mailing_address_same && (
+          <AddressSelector
+            handleChangeAddress={handleMailingAddressChange}
+            name={'LOCAL_mailing_address_verification_selection'}
+            legend={t('address_verification.legend.mailing')}
+            options={mailingAddressOptions}
+          />
+        )}
+      </>
+    )
+  }
+  //default state, covers if there is an error in either verification response, or if there were no matches from the verification check
   return (
     <>
-      <AddressSelector
-        handleChangeAddress={handleResidenceAddressChange}
-        name={'LOCAL_residence_address_verification_selection'}
-        legend={t('address_verification.legend.residence')}
-        options={residenceAddressOptions}
-      />
-      {!values.LOCAL_mailing_address_same && (
-        <AddressSelector
-          handleChangeAddress={handleMailingAddressChange}
-          name={'LOCAL_mailing_address_verification_selection'}
-          legend={t('address_verification.legend.mailing')}
-          options={mailingAddressOptions}
-        />
-      )}
-      <ClaimFormButtons nextStep={nextPage.heading}>
-        <BackButton previousPage={previousPage.path} />
-        <NextButton nextPage={nextPage.path} />
-      </ClaimFormButtons>
+      <Alert type={'warning'} headingLevel="h2" role="alert" slim>
+        {values.LOCAL_mailing_address_same &&
+          t('address_verification.same_address.no_match')}
+        {!values.LOCAL_mailing_address_same &&
+          t('address_verification.distinct_addresses.no_match')}
+      </Alert>
+      <p>
+        {values.LOCAL_mailing_address_same &&
+          t('address_verification.same_address.entered')}
+        {!values.LOCAL_mailing_address_same &&
+          t('address_verification.distinct_addresses.entered')}
+      </p>
+      <CardGroup
+        className="flex-column margin-top-105 margin-bottom-1"
+        data-testid="entered_addresses_card_group"
+      >
+        <Card className="margin-bottom-105">
+          {formattedAddress(ENTERED_RESIDENTIAL_ADDRESS, true)}
+        </Card>
+        {!values.LOCAL_mailing_address_same && (
+          <Card className="margin-bottom-0">
+            {formattedAddress(ENTERED_MAILING_ADDRESS, true)}
+          </Card>
+        )}
+      </CardGroup>
+      <p>
+        {values.LOCAL_mailing_address_same && (
+          <Trans t={t} i18nKey={'address_verification.same_address.proceed'}>
+            <u>{''}</u>
+          </Trans>
+        )}
+        {!values.LOCAL_mailing_address_same && (
+          <Trans
+            t={t}
+            i18nKey={'address_verification.distinct_addresses.proceed'}
+          >
+            <u>{''}</u>
+          </Trans>
+        )}
+      </p>
     </>
   )
 }
@@ -166,20 +219,26 @@ const AddressSelector = ({
         return {
           value: option.value,
           label: option.label,
-          labelDescription: (
-            <>
-              <div>
-                <div>{option.address?.address}</div>
-                <div>
-                  {option.address?.city}, {option.address?.state}{' '}
-                  {option.address?.zipcode}
-                </div>
-              </div>
-            </>
-          ),
+          labelDescription: formattedAddress(option.address),
         }
       })}
     />
+  )
+}
+
+const formattedAddress = (address: AddressInput, isCardContent?: boolean) => {
+  return (
+    <div
+      className={`${
+        isCardContent ? 'padding-bottom-105 padding-top-105 padding-left-3' : ''
+      }`}
+    >
+      <div>{address.address}</div>
+      {address.address2 && <div>{address.address2}</div>}
+      <div>
+        {address.city}, {address.state} {address.zipcode}
+      </div>
+    </div>
   )
 }
 
@@ -192,6 +251,10 @@ const AddressVerification: NextPageWithLayout = () => {
       index={pageDefinitions.indexOf(pageDefinition)}
     >
       <AddressVerificationFeedback />
+      <ClaimFormButtons nextStep={nextPage.heading}>
+        <BackButton previousPage={previousPage.path} />
+        <NextButton nextPage={nextPage.path} />
+      </ClaimFormButtons>
     </ClaimFormik>
   )
 }
